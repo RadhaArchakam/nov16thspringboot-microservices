cb766c810690b0f328854e8629a099132299762b
RadhaArchakam <151070507+RadhaArchakam@users.noreply.github.com>
2023-11-17 12:48:49 +0530

    Update ESchoolApplication.java

--- e-school/src/main/java/com/cts/eschool/ESchoolApplication.java
+++ e-school/src/main/java/com/cts/eschool/ESchoolApplication.java
@@ -12,14 +12,14 @@ public class ESchoolApplication {
 
 		ConfigurableApplicationContext ss = SpringApplication.run(ESchoolApplication.class, args);
 		StudentService s = ss.getBean(StudentService.class);
-		s.addNewStudentDetail(1, "Tarun", 78);
-		s.addNewStudentDetail(2, "Ganesh", 45);
-		s.addNewStudentDetail(3, "Abhi", 98);
+		s.addNewStudentDetail(101, "Radha", 78);
+		s.addNewStudentDetail(201, "Anand", 45);
+		s.addNewStudentDetail(301, "Ahalya", 98);
 
 		s.getAllStudentDetails();
-		s.getStudentDetailsById(2);
-		s.updateStudentScore(2,50.8);
-		s.deleteStudentDetails(1);
+		s.getStudentDetailsById(201);
+		s.updateStudentScore(201,50.8);
+		s.deleteStudentDetails(101);
 		s.getAllStudentDetails();
 	}
 
