91163830f943fbd0d76b6f7b6b687db9e6d127a6
RadhaArchakam <151070507+RadhaArchakam@users.noreply.github.com>
2023-11-20 12:30:27 +0530

    Assignment2-Autowired

--- SchoolManagement/src/main/java/com/cognizant/school/SchoolManagementApplication.java
+++ SchoolManagement/src/main/java/com/cognizant/school/SchoolManagementApplication.java
@@ -2,7 +2,6 @@ package com.cognizant.school;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
